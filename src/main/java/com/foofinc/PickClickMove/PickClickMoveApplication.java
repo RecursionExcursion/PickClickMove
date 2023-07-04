f27428c91db790d5a22cbee573638c07cb428c2c
RecursionExcursion <rloup15@gmail.com>
2023-07-04 12:53:59 -0400

    Initial commit

---
+++ src/main/java/com/foofinc/PickClickMove/PickClickMoveApplication.java
@@ -0,0 +1,12 @@
+package com.foofinc.PickClickMove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickClickMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickClickMoveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/foofinc/PickClickMove/PickClickMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foofinc.PickClickMove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickClickMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
